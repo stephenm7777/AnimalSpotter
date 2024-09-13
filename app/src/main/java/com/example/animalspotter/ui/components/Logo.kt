95189218c002d06dda2f3b2d6241862b49b7981b
Stephen Martinez <122754065+stephenm7777@users.noreply.github.com>
2024-09-12 21:07:50 -0700

    Sign up page
    Added logo

--- app/src/main/java/com/example/animalspotter/ui/components/Logo.kt
+++ app/src/main/java/com/example/animalspotter/ui/components/Logo.kt
@@ -4,8 +4,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -26,6 +28,24 @@ fun LogoImage(modifier: Modifier = Modifier) {
                 .size(320.dp)
                 .align(Alignment.Center)
                 .offset(y = (-100).dp)
+        )
+    }
+}
+
+@Composable
+fun Symbol(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.biospot_favicon_color)
+    Box(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = image,
+            contentDescription = "symbol",
+            modifier = Modifier
+                .size(100.dp)
+                .align(Alignment.TopEnd)
         )
     }
 }
--- app/src/main/java/com/example/animalspotter/ui/screens/SignUp.kt
+++ app/src/main/java/com/example/animalspotter/ui/screens/SignUp.kt
@@ -12,7 +12,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,11 +19,14 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.animalspotter.ui.components.HomeButton
-import com.example.animalspotter.ui.components.LogoImage
 import com.example.animalspotter.ui.components.SubmitButton
+import com.example.animalspotter.ui.components.Symbol
 import com.example.animalspotter.ui.theme.CustomRed
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -49,7 +51,7 @@ fun SignUp(navController: NavHostController) {
                 })
             }
             .background(Color.White)
-            .offset(y = 100.dp),
+            .offset(y = 75.dp),
         contentAlignment = Alignment.Center
     ){
 
@@ -60,11 +62,33 @@ fun SignUp(navController: NavHostController) {
             verticalArrangement = Arrangement.spacedBy(8.dp)
 
         ) {
+            Text(
+                text = "Welcome",
+                style = TextStyle(
+                    fontSize = 32.sp,
+                    fontWeight = FontWeight.SemiBold,
+                    color = CustomRed
+                ),
+                modifier = Modifier
+                    .padding(bottom = 5.dp)
+                    .offset(y=(-100).dp)
+            )
+            Text(
+                text = "Thank you for signing up",
+                style = TextStyle(
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = CustomRed
+                ),
+                modifier = Modifier
+                    .padding(bottom = 10.dp)
+                    .offset(y=(-100).dp)
+            )
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .background(Color.White, RoundedCornerShape(33.dp))
-                    .border(1.dp, Color.Gray, RoundedCornerShape(33.dp)) // Thin black border
+                    .border(1.dp, Color.Gray, RoundedCornerShape(33.dp))
                     .clip(RoundedCornerShape(33.dp))
             ){
                 TextField(
@@ -155,6 +179,12 @@ fun SignUp(navController: NavHostController) {
             SubmitButton(navController = navController)
             HomeButton(navController = navController)
         }
+        Symbol(
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(16.dp)
+                .offset(y=(-70).dp, x = 10.dp)
+        )
     }
 
 }
